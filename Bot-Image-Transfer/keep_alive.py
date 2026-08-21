e5281c24f9af8384539c64ef1c7593270094fe51
endressponse-dot <endressponse@gmail.com>
2026-08-21 16:55:17 +0900

    Update keep_alive.py

--- Bot-Image-Transfer/keep_alive.py
+++ Bot-Image-Transfer/keep_alive.py
@@ -3,7 +3,6 @@ import logging
 from flask import Flask
 from threading import Thread
 
-# ログ表示を整理
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
@@ -14,7 +13,6 @@ def home():
     return "Bot status: ONLINE"
 
 def run():
-    # Renderが指定するPORT環境変数を優先的に取得（標準は10000）
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
