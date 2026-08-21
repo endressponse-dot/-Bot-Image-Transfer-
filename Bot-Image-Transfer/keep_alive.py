c5746a2acbdc2a7bd933e800c75a0bef15ea0d95
endressponse-dot <endressponse@gmail.com>
2026-08-21 16:02:31 +0900

    Update keep_alive.py

--- Bot-Image-Transfer/keep_alive.py
+++ Bot-Image-Transfer/keep_alive.py
@@ -1,20 +1,24 @@
 import os
+import logging
 from flask import Flask
 from threading import Thread
 
-app = Flask(__name__)
+# ログ表示を整理
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is active!"
+    return "Bot status: ONLINE"
 
 def run():
-    # Renderが指定するPORT環境変数を取得（無ければ10000番ポート）
+    # Renderが指定するPORT環境変数を優先的に取得（標準は10000）
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
-    """Botとは別にバックグラウンドでWebサーバーを起動する"""
     t = Thread(target=run)
     t.daemon = True
     t.start()
